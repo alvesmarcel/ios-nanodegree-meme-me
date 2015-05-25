2b53af71d76f466db2f0f11138a9565ba453723c
Marcel Oliveira Alves <alvesmarcel@Marcels-MacBook-Pro.local>
2015-05-25 13:06:53 -0300

    SentMemesCollectionVC comments added; MemeCollectionViewCell comments added

--- MemeMe/MemeCollectionViewCell.swift
+++ MemeMe/MemeCollectionViewCell.swift
@@ -5,6 +5,8 @@
 //  Created by Marcel Oliveira Alves on 5/24/15.
 //  Copyright (c) 2015 Marcel Oliveira Alves. All rights reserved.
 //
+//  Custom cell to be used in a Collection View
+//  This was created to add the delete label and make it clear that the user is in editing (deleting) mode
 
 import UIKit
 
--- MemeMe/SentMemesCollectionVC.swift
+++ MemeMe/SentMemesCollectionVC.swift
@@ -5,13 +5,18 @@
 //  Created by Marcel Oliveira Alves on 5/24/15.
 //  Copyright (c) 2015 Marcel Oliveira Alves. All rights reserved.
 //
+//  This ViewController is responsible for the collection exibition of the sent (shared) memes. This class:
+//  - Displays the memes in a collection
+//  - Performs a push transition to MemeDetailVC if an item is selected
+//  - Deletes the meme from the collection and from the model (AppDelegate's memes array)
 
 import UIKit
 
 class SentMemesCollectionVC: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
   
-  var memes: [Meme]!
-  var collectionViewIsEditing: Bool!
+  var memes: [Meme]! // memes that will be shown in the collection
+  var collectionViewIsEditing: Bool! // indicates if the view is in "editing mode" (collectionViewIsEditing == true)
+  
   @IBOutlet weak var collectionView: UICollectionView!
   
   override func viewDidLoad() {
@@ -34,7 +39,7 @@ class SentMemesCollectionVC: UIViewController, UICollectionViewDataSource, UICol
     
     // collectionView configuration and reloading - needed to update the items
     collectionView.backgroundColor = UIColor.whiteColor()
-    collectionViewIsEditing = false
+    collectionViewIsEditing = false // editing mode is always disabled when the view appears
     collectionView.reloadData()
   }
   
@@ -54,7 +59,9 @@ class SentMemesCollectionVC: UIViewController, UICollectionViewDataSource, UICol
     return cell
   }
   
+  // Performs different actions depending on the collectionViewIsEditing
   func collectionView(collectionView: UICollectionView, didSelectItemAtIndexPath indexPath: NSIndexPath) {
+    // If it is in editing mode, this method should delete the meme; otherwise, it should push the MemeDetailVC
     if (collectionViewIsEditing == true) {
       deleteMeme(indexPath.item)
     } else {
@@ -65,17 +72,19 @@ class SentMemesCollectionVC: UIViewController, UICollectionViewDataSource, UICol
     }
   }
   
+  // Enables editing mode when the Edit button is tapped
   @IBAction func enableEdit(sender: AnyObject) {
     collectionViewIsEditing = !collectionViewIsEditing
     collectionView.reloadData()
   }
   
+  // Performs the deletion of a meme from the collection view and from the model (AppDelegate's memes array)
   func deleteMeme(index: Int) {
     let object = UIApplication.sharedApplication().delegate
     let appDelegate = object as! AppDelegate
     appDelegate.memes.removeAtIndex(index)
     
-    // Resolving view problem
+    // Resolving view problem (it seems that UICollectionView doesn't handle this automatically)
     memes.removeAtIndex(index)
     collectionView.reloadData()
   }
