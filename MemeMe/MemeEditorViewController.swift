c05168cc85f365f5ed10fa3c3a8ee0de3574be45
Marcel Oliveira Alves <marcel.alves89@gmail.com>
2016-03-17 23:54:58 -0300

    docs: Add an important TODO.

--- MemeMe/MemeEditorViewController.swift
+++ MemeMe/MemeEditorViewController.swift
@@ -18,6 +18,8 @@ import UIKit
 
 class MemeEditorViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate, UITextFieldDelegate {
 	
+	// TODO: Change top bar to a navigation bar
+	
 	// MARK: Outlets
 	
 	@IBOutlet weak var imagePickerView: UIImageView!
